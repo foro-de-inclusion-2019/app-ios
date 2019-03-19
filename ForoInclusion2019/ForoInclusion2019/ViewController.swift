83c8ffb6e50b869644d88c7f45d8ed7401167fe1
Alumno <alumno@Laboratorios-MacBook-Pro21.local>
2019-03-19 16:26:55 -0600

    Set up xcode project

---
+++ ForoInclusion2019/ForoInclusion2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForoInclusion2019
+//
+//  Created by Alumno on 3/19/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
