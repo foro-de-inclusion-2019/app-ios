0ddf2a28296c9d064e7884c9c4766001a1fb7235
Alumno <alumno@Laboratorios-MacBook-Pro21.local>
2019-03-19 17:29:10 -0600

    begin ui design

---
+++ ForoInclusion2019/ForoInclusion2019/TableViewCellEvento.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCellEvento.swift
+//  ForoInclusion2019
+//
+//  Created by Alumno on 3/19/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCellEvento: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
