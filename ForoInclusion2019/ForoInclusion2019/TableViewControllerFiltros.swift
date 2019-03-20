bf7e892b968d08675648c4f8b9447421035e8750
Mauricio <mauricio.gm@outlook.com>
2019-03-20 14:21:44 -0600

    added filters view

---
+++ ForoInclusion2019/ForoInclusion2019/TableViewControllerFiltros.swift
@@ -0,0 +1,108 @@
+//
+//  TableViewControllerFiltros.swift
+//  ForoInclusion2019
+//
+//  Created by Alumno on 3/20/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class TableViewControllerFiltros: UITableViewController {
+
+    // Temporal... se usaran enums para identificar esto
+    var ambitos = ["Social", "Laboral", "Salud", "Escolar"]
+    var tipos = ["Auditiva", "Visual", "Psicosocial", "Motriz", "Intelectual"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        let btnAplicar = UIBarButtonItem(title: "Aplicar", style: .done, target: nil, action: nil)
+        self.navigationItem.rightBarButtonItem = btnAplicar
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "Ámbitos"
+        } else {
+            return "Tipos de discapacidad"
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        if section == 0 {
+            return ambitos.count
+        } else {
+            return tipos.count
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "celdaFiltro", for: indexPath)
+
+        if indexPath.section == 0 {
+            cell.textLabel?.text = ambitos[indexPath.row]
+        } else {
+            cell.textLabel?.text = tipos[indexPath.row]
+        }
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
