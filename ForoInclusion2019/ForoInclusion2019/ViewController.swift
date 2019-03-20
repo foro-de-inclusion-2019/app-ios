549dc21b5d847f102b8b0cf4c4e513ef87eb560c
Alumno <alumno@Laboratorios-MacBook-Pro-14.local>
2019-03-20 16:50:12 -0600

    Added support for HighContrast, VoiceOver, DynamicText

--- ForoInclusion2019/ForoInclusion2019/ViewController.swift
+++ ForoInclusion2019/ForoInclusion2019/ViewController.swift
@@ -14,7 +14,53 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    /*
+     * Method which receives a functionality name and appends it to alert, showing the path in which you can activate it.
+     */
+    func showAlert( functionality: String ){
+        
+        let alertController = UIAlertController (title: functionality, message: "Go to Settings > General > " + functionality, preferredStyle: .alert)
+        
+        let settingsAction = UIAlertAction(title: "Settings", style: .default) { (_) -> Void in
+            
+            guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+                return
+            }
+            
+            if UIApplication.shared.canOpenURL(settingsUrl) {
+                UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                    print("Settings opened: \(success)") // Prints true
+                    print(settingsUrl)
+                })
+            }
+        }
+        alertController.addAction(settingsAction)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
 
+    /* Leftmost button is clicked */
+    @IBAction func activateHighContrast(_ sender: Any) {
+        
+        showAlert(functionality: "Increase Contrast")
+    
+    }
 
+    /* Middle button is clicked */
+    @IBAction func activateVoiceOver(_ sender: Any) {
+        
+        showAlert(functionality: "Speech")
+        
+    }
+    
+    /* Rightmost button is clicked */
+    @IBAction func activateBigLetters(_ sender: Any) {
+    
+        showAlert(functionality: "Larger Text")
+    
+    }
 }
 
